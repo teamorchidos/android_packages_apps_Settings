043255ed5c215674e969505ee93f794ca02be7f7
Sujit Roy <kumarsujitroy@gmail.com>
2020-04-24 06:06:44 +0000

    Revert "[4/4] Port face unlock feature"
    This reverts commit 867e8996fb9bc748d8f6d4824cf093627ae44814.

--- Android.mk
+++ Android.mk
@@ -51,8 +51,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     contextualcards \
     settings-logtags \
     zxing-core-1.7 \
-    org.lineageos.platform.internal \
-    faceunlock_utils
+    org.lineageos.platform.internal
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
