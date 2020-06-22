0e792529540fed5074083dd78492631d10eb0b8f
ShanuDey <sd4shanudey@gmail.com>
2020-06-22 21:09:23 +0530

    Settings: [4/4] Port face unlock feature
    This reverts commit 043255ed5c215674e969505ee93f794ca02be7f7.
    
    Change-Id: I84c9af465b1256cfc623faff990903be5cdcb3bd

--- Android.mk
+++ Android.mk
@@ -51,7 +51,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     contextualcards \
     settings-logtags \
     zxing-core-1.7 \
-    org.lineageos.platform.internal
+    org.lineageos.platform.internal \
+    faceunlock_utils
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
